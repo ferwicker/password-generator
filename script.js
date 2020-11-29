a17b650cd997240a9b93c29db4c965f758dc639e
Fer Wicker <fer.wicker@gmail.com>
2020-11-29 16:15:53 +1100

    defining possible characters with prompts

--- script.js
+++ script.js
@@ -16,21 +16,34 @@ console.log(lowercase + uppercase + numbers + symbols);
 
 function writePassword() {
 
-  pwdLength = parseInt(prompt('Enter your password length. (Must be between 8 and 128)'));
+  pwdLength = parseInt(prompt('Enter your password length. (Must be between 8 and 128 characters)'));
 
   console.log("Password length: " + pwdLength);
 
   if (pwdLength >= 8 && pwdLength <= 128){
 
+    var characters =
+        confirm("Include lower case letters?") ? lowercase : '';
+    characters =
+        confirm("Include upper case letters?") ? characters + uppercase : characters;
+    characters =
+        confirm("Include numbers?") ? characters + numbers : characters;
+    characters =
+        confirm("Include special characters?") ? characters + symbols : characters;
+    
+    
+    console.log(characters);
+
     var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+
+
+    var passwordText = document.querySelector("#password"); 
   
     passwordText.value = password;
 
   } else {
-    alert("Invalid password length!")
+    alert("Invalid password length. Please try again.")
   }
-
 
 }
 
