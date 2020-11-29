e59142f5ba1fa51acad2ae6540908011ef6e2884
Fer Wicker <fer.wicker@gmail.com>
2020-11-29 15:29:06 +1100

    added possible password characters

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+// possible characters
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~"; //does not include "" and \
+var pwdLength = 8;
+
+console.log(lowercase + uppercase + numbers + symbols);
+
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -20,7 +32,7 @@ when generateBtn is clicked, run writePassword function
   writePassword function 
   length = prompt length
   change string to number
-  check length with min-max otherwise invalid, ask again?
+  check length with min-max otherwise invalid, ask again? or go to a default?
   confirm or select criteria...
     lowercase (arrays?)
     uppercase
@@ -28,7 +40,7 @@ when generateBtn is clicked, run writePassword function
     special characters
   how to check if at least one is selected...
   
-    then generate password?
+    then generatePassword?
     use length chosen to pick random characters (for loop)
 
     then display result to screen (inner html?)
